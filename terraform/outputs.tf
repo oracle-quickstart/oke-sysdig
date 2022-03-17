aa9d5293a02b19eb911f50d1ac577b5a8a3e8ffc
junior <junior@users.noreply.github.com>
2022-03-17 17:21:33 -0500

    providers and version
    Signed-off-by: junior <junior@users.noreply.github.com>

---
+++ terraform/outputs.tf
@@ -0,0 +1,17 @@
+# Copyright (c) 2022 Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+# 
+
+output "dev" {
+  value = "Made with \u2764 by Oracle Developers and Sysdig"
+}
+output "deploy_id" {
+  value = random_string.deploy_id.result
+}
+output "deployed_to_region" {
+  value = var.region
+}
+
+output "stack_version" {
+  value = file("${path.module}/VERSION")
+}
